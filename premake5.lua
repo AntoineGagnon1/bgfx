ed2ead8437cfdca991e8bfda8a88155d0cce327b
Antoine Gagnon <antoinegagnon123@gmail.com>
2024-09-20 21:34:17 -0400

    add premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,19 @@
+externalproject "bgfx"
+	location ".build/projects/vs2022/"
+	uuid "6C90947C-58C7-950D-01B4-7B10EDC9110F"
+	kind "StaticLib"
+	language "C++"
+	configmap {
+		["Final"] = "Release",
+		["Win64"] = "x64"
+	}
+	
+externalproject "bx"
+	location ".build/projects/vs2022/"
+	uuid "5F775900-4B03-880B-B4B1-880BA05C880B"
+	kind "StaticLib"
+	language "C++"
+	configmap {
+		["Final"] = "Release",
+		["Win64"] = "x64"
+	}
